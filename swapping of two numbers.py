d0c3b083d27e2d6867161d9376a7f1a2cea8f6f6
Zuneranawaz <shaikzunara@gmail.com>
2024-07-17 21:38:34 +0530

    swapping of two numbers.py

--- swapping of two numbers.py
+++ swapping of two numbers.py
@@ -1,10 +1,10 @@
-a=int(input("enter a")
-b=int(input("enter b")
+a=int(input("enter a"))
+b=int(input("enter b"))
 print("before swapping:")
 print("a=",a)
 print("b=",b)
 a=a+b
-a=a-b
+b=a-b
 a=a-b
 print("after swapping:")
 print("a=",a)
