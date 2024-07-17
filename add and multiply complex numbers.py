bf64fab0153fb136a654743a7c93aba1476a5496
Zuneranawaz <shaikzunara@gmail.com>
2024-07-17 21:21:50 +0530

    add and multiply complex numbers.py

---
+++ add and multiply complex numbers.py
@@ -0,0 +1,5 @@
+# program to add and multiply two complex numbers
+a=int(input("enter the first complex number(i.e in format a+jb)":))
+b=int(input("enter the second complex number(i.e in format a+jb)"))
+print("addition",a+b)
+print("multiplication",a*b)
